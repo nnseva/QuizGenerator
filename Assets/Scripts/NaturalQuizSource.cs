b71e504ff0f92db9fdc2b43ff9d9fa056c0105e8
Vsevolod Novikov <nnseva@gmail.com>
2020-05-13 21:47:22 +0300

    Fixed digits parameter

--- Assets/Scripts/NaturalQuizSource.cs
+++ Assets/Scripts/NaturalQuizSource.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 
 abstract public class NaturalQuizSource : QuizSource {
     // Natural counting quiz source
-    public int digits;
+    [Tooltip("Number of digits in the test values")]
+    public int Digits = 1;
     public override string category { get { return GetLocalizedString("Natural numbers"); } }
-    public override string theme { get { return GetLocalizedString("Performing {0} of {1}-digits numbers", action_name, digits); } }
+    public override string theme { get { return GetLocalizedString("Performing {0} of {1}-digits numbers", action_name, Digits); } }
     virtual public int a(int id) {
-        return (int)(id / Mathf.Pow(10f, (float)digits));
+        return (int)(id / Mathf.Pow(10f, (float)Digits));
     }
     virtual public int b(int id) {
-        return (int)(id % Mathf.Pow(10f, (float)digits));
+        return (int)(id % Mathf.Pow(10f, (float)Digits));
     }
     public override int random_id() {
-        float mn = Mathf.Pow(10f, (float)(digits - 1));
-        float mx = Mathf.Pow(10f, (float)(digits));
+        float mn = Mathf.Pow(10f, (float)(Digits - 1));
+        float mx = Mathf.Pow(10f, (float)(Digits));
         int x = (int)Random.Range(mn, mx);
         int y = (int)Random.Range(mn, mx);
-        return x * (int) Mathf.Pow(10f, digits) + y;
+        return x * (int) Mathf.Pow(10f, Digits) + y;
     }
     public override string question(int id) {
         return GetLocalizedString("How much will {0} {1} {2}?", a(id), action_sign, b(id));
