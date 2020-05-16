36deeab896fb6c520469ee8ea1f6454260113400
Vsevolod Novikov <nnseva@gmail.com>
2020-05-16 21:47:00 +0300

    Package moved to the GitHub

--- Packages/com.nnseva.quiz/Runtime/Assets/Scripts/NaturalPlusQuizSource.cs
+++
@@ -1,6 +0,0 @@
-public class NaturalPlusQuizSource : NaturalQuizSource {
-    public override string action_sign { get { return "+"; }}
-    public override string action_name { get { return GetLocalizedString("addition"); }}
-    public override string quiz_name { get { return "Addition"; }}
-    public override int action(int a, int b) { return a + b; }
-}
